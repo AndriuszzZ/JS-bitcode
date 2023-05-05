8251a6fe1f0bd2a15b54466fb431bcf2409b6e28
AndriuszzZ <andre.genolima@gmail.com>
2023-05-04 21:53:56 -0300

    teste e Exercicio1

---
+++ Exercicio1/recrutas.js
@@ -0,0 +1,11 @@
+const primeiroNome = prompt("informe o primeiro nome do recruta:")
+const sobrenome = prompt("Informe o sobrenome do recruta:")
+const campoDeEstudo = prompt("Qual é o campo de esdudo do recruta?")
+const anoNascimento = prompt("Qual é o ano de nascimento do recruta?")
+
+alert(
+    "recruta cadastrado com sucesso!\n" + 
+    "\nnome completo: " + primeiroNome + " " + sobrenome + 
+    "\ncampo de estudo: " + campoDeEstudo + 
+    "\nIdade: " + (2023 - anoNascimento)
+)
---
+++ conhecendo o JS/RecursosNativosBrowser/index.js
@@ -0,0 +1,12 @@
+let nome = "André"
+
+console.log(nome)
+console.error(nome)
+console.warn(nome)
+
+// window.alert(nome)
+// nome = window.prompt("insira o seu nome:")
+// let confirmacao = window.confirm("Confirma essa ação?")
+alert("bem Vindo")
+prompt("Olá")
+confirm("Confirma?")
