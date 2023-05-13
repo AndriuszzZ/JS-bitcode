f5ea84ba05acbb68cdf3b9985b6e7ccd749f3c86
AndriuszzZ <andre.genolima@gmail.com>
2023-05-12 21:55:15 -0300

    teste

---
+++ conhecendo o JS/For/index.js
@@ -0,0 +1,8 @@
+// inicialização ; condição ; finalização
+
+let nome = "André" // tamanho 5 -> 0, 1, 2, 3, 4
+
+for (let indice = 0; indice < nome.length; indice++) {
+    console.log(nome[indice])
+}
+
