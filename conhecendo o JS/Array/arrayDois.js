9384452ae83596bb7af5caee0f35194595c00c30
AndriuszzZ <andre.genolima@gmail.com>
2023-05-18 21:54:25 -0300

    teste

---
+++ conhecendo o JS/Array/arrayDois.js
@@ -0,0 +1,16 @@
+const arr = ["Frodo", "Sam", "Merry", "Pippin", "Gandalf", "Aragorn", "Legolas", "Gimli"]
+
+// Adicionar elementos
+// push
+// arr.push("Boromir")
+// console.log(arr)
+
+// // unshift
+// arr.unshift("Boromir")
+// console.log(arr)
+
+// Remover elementos
+// pop
+const ultimoElemento = arr.pop("")
+console.log(arr)
+console.log(ultimoElemento)
