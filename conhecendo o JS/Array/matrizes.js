f423f8571ab31a518673fcd2a41bd9bcf64d0c3c
AndriuszzZ <andre.genolima@gmail.com>
2023-06-09 22:31:29 -0300

    arrays matriz

--- conhecendo o JS/Array/matrizes.js
+++ conhecendo o JS/Array/matrizes.js
@@ -17,3 +17,12 @@ console.log(arr[2])
 console.log(arr[2][1])
 console.log(arr[2][1][0])
 
+const matriz = [
+    ["l1, c1", "l1, c2", "l1, c3", "l1, c4"],
+    ["l2, c1", "l2, c2", "l2, c3", "l2, c4"],
+    ["l3, c1", "l3, c2", "l3, c3", "l3, c4"],
+  ]
+
+  // Vai tentar manipular o valor em tabela
+  // table
+  console.table(matriz)
