d6a682c574bc1cb25f554a99fdb0ea968719fda1
AndriuszzZ <andre.genolima@gmail.com>
2023-07-23 22:24:17 -0300

    eventos html

---
+++ 2-DOM/Eventos HTML/eventos.js
@@ -0,0 +1,19 @@
+function mouseEnterEvent() {
+    console.log('Entrada do mouse')
+  }
+  
+  function changeEvent() {
+    console.log('Mudança no valor')
+  }
+  
+  function focusEvent() {
+    console.log('Foco no elemento')
+  }
+  
+  function blurEvent() {
+    console.log('Desfocar o elemento')
+  }
+  
+  function clickEvent() {
+    console.log('Clicou no botão')
+  }
